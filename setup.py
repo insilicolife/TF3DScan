8482fda2fae74f28714074a10cd027ac04b93745
Cloud User <cloud-user@trapist-2.novalocal>
2018-05-28 12:50:11 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='tf3dscan',
+      version='0.1',
+      description='Transcription factor binding site prediction using 3D convolutional scan',
+      url='http://github.com/storborg/funniest',
+      author='Nigatu Ayele',
+      author_email='naodm2006awet4@gmail.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
